ef1b331b75a2a205c8311e706e2499155dacbf4a
saad <saad.fatihi9@gmail.com>
2021-10-03 22:36:50 +0200

    development of the Gameover page

--- src/components/pages/GameOver/GameOver.tsx
+++ src/components/pages/GameOver/GameOver.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { ScoreContext } from "../../../context/scoreContext";
 import { Title } from "../../atoms/Title";
 import {
   Container,
@@ -11,33 +14,57 @@ import {
 } from "./GameOver.styled";
 
 const GameOver = () => {
+  const history = useHistory();
+  const { result, results, saveResult } = useContext(ScoreContext);
+  const [input, setInput] = useState("");
+  const [display, setDisplay] = useState(true);
+
   return (
     <Container>
-      <Title>Game over</Title>
-      <ScoreContainer>
-        <Label>Time spent in the game</Label>
-        <Label>15 s</Label>
-      </ScoreContainer>
-      <ScoreContainer>
-        <Label>Good answers</Label>
-        <Label>10 good answers</Label>
-      </ScoreContainer>
-      <TryButton>Try again</TryButton>
-      <Title>Highscore</Title>
-      <ScoreContainer>
-        <Label>Lorem ipsum</Label>
-        <Label>15 s</Label>
-        <Label>10 good answers</Label>
-      </ScoreContainer>
-      <ScoreContainer>
-        <Label>Lorem ipsum</Label>
-        <Label>15 s</Label>
-        <Label>10 good answers</Label>
-      </ScoreContainer>
-      <InputContainer>
-        <NameInput />
-        <AddButton>Add</AddButton>
-      </InputContainer>
+      {result && (
+        <>
+          <Title>{result.score === 10 ? "Well done" : "Game over"}</Title>
+          <ScoreContainer>
+            <Label>Time spent in the game</Label>
+            <Label>{`${result.seconds} s`}</Label>
+          </ScoreContainer>
+          <ScoreContainer>
+            <Label>Good answers</Label>
+            <Label>{`${result.score} good answer(s)`}</Label>
+          </ScoreContainer>
+        </>
+      )}
+
+      <TryButton onClick={() => history.push("/survey")}>Try again</TryButton>
+      {results.length > 0 && <Title>Highscore</Title>}
+      {results.slice(0, 10).map((r, index) => (
+        <ScoreContainer key={index}>
+          <Label>{r.name}</Label>
+          <Label>{`${r.seconds} s`}</Label>
+          <Label>{`${r.score} good answer(s)`}</Label>
+        </ScoreContainer>
+      ))}
+
+      {display && result && (
+        <>
+          <InputContainer>
+            <NameInput
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <AddButton
+              disabled={input.length === 0}
+              onClick={() => {
+                saveResult({ ...result, name: input });
+                setInput("");
+                setDisplay(false);
+              }}
+            >
+              Add
+            </AddButton>
+          </InputContainer>
+        </>
+      )}
     </Container>
   );
 };
